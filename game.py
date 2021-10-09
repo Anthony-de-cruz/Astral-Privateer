cc4046a298ee475aa4d17c098d1d052deafe006b
Anthony de Cruz <anthonydaviddecruz@gmail.com>
2021-10-09 15:22:23 +0100

    added main loop

--- game.py
+++ game.py
@@ -27,14 +27,23 @@ class Game():
     def __init__(self,
         window_name: str,
         window_width: int,
-        window_height: int):
+        window_height: int,
+        COLOUR_PALETTE: dict,
+        frame_rate: int):
 
+        self.COLOUR_PALETTE = COLOUR_PALETTE
+        self.clock = pygame.time.Clock()
+        self.frame_rate = frame_rate
         self.window_name = window_name
 
         self.window_width = window_width
         self.window_height = window_height
+
+        self.objects: list[GameObject] = []
         
         self.setup_window()
+
+        self.running = True
     
     def setup_window(self) -> None:
 
@@ -44,3 +53,39 @@ class Game():
             (self.window_width, self.window_height))
         pygame.display.set_caption((self.window_name))
         os.environ["SDL_VIDEO_CENTERED"] = "1"
+
+    def add_object(self, game_object: GameObject) -> None:
+
+        """Method to add a game object into the game object list"""
+
+        self.objects.append(game_object)
+    
+    def draw_objects(self) -> None:
+
+        """Method to draw all objects"""
+
+        for obj in self.objects:
+            obj.draw(self.window, self.COLOUR_PALETTE)
+
+    def handle_events(self) -> None:
+
+        """Method to handle pygame events"""
+
+        for event in pygame.event.get():
+
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+    
+    def main_loop(self) -> None:
+
+        """Method containing the main loop"""
+
+        while self.running:
+
+            self.clock.tick(self.frame_rate)
+            self.handle_events()
+
+            self.draw_objects()
+            pygame.display.flip()
+
--- main.py
+++ main.py
@@ -12,19 +12,39 @@ class AstralPrivateer(Game):
     def __init__(self,
         window_name: str,
         window_width: int,
-        window_height: int
+        window_height: int,
+        COLOUR_PALETTE: dict,
+        frame_rate: int
         ):
         super().__init__(
             window_name,
             window_width,
-            window_height)
+            window_height,
+            COLOUR_PALETTE,
+            frame_rate)
+
+    def draw_objects(self) -> None:
+
+        """Method to draw all objects"""
+
+        self.window.fill(self.COLOUR_PALETTE["Black"])
+
+        for obj in self.objects:
+            obj.draw(self.window, self.COLOUR_PALETTE)
         
 
 def main() -> None:
 
+    COLOURS = {
+        "Black": (0, 0, 0),
+        "White": (255, 255, 255)
+    }
+
     pygame.init()
 
-    game = AstralPrivateer("Astral Privateer", 800, 800)
+    game = AstralPrivateer("Astral Privateer", 800, 800, COLOURS, 60)
+
+    game.main_loop()
 
 if __name__ == "__main__":
 
